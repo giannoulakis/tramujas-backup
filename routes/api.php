b2c563d2f4a3aa213f2b17b678748e6ba8f1a22a
Patrick Scheibel <patrick@ideatos.com.br>
2021-10-18 16:34:55 -0300

    Migracao dos routes

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98aafb07135bb4e3ae879439fd2c326b395dec808226415339e295641fe9970c
-size 1071
+oid sha256:fc70f27e0d9d0822f4c6c36576eb09d7ff217a3ae2033ef465b6553cca550f9c
+size 10150
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9199e2056a0bbe38cdb4fc95214b7e0984be78908a6edd5c8ddbc37238eb4eb
-size 1641
+oid sha256:54184bc6b1568549c3e63a4b422c81ae9089e637d1b053328d4a231d3ae5a512
+size 9449
