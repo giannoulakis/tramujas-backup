41071705360e15d94b3a9e97e5de663447cd17bc
Patrick Scheibel <patrick@ideatos.com.br>
2021-10-26 18:33:18 -0300

    Adicionada rotas de entidade e ajuste no composer

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5542a32ab8c87426d17e10482670a337bb4f481c0f59bc3594fcd516ae3cb8db
-size 9971
+oid sha256:035729b3a45f3a42d196a10194bf2549b2ecb47a9d632579bd5fdf5808686076
+size 10148
