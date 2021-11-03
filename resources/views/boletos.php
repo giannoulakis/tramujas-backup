416ea143d5e5ac1295db298b01579b4798e8083f
Patrick Scheibel <patrick@ideatos.com.br>
2021-11-03 14:59:18 -0300

    Migracao incompleta do controller e view de boleto

--- resources/views/boletos.php
+++ resources/views/boletos.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6151c8f8fa4714ef8ef2878efe2b2bdd9361b83898d5953e52f357f0f96ca01
-size 18354
+oid sha256:aeb9ebd972a1019cca38f5552b719e34331f5b89e4e19da710c23eb53f076c00
+size 18305
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1376e3502b56578ccbed7dc96b1c7f8ca7c082bfac9570989744de83fa092a17
-size 10335
+oid sha256:ee90e0b19fffd165c94b1f5ebf8e87ecbf6b1f29ef5355e97e38ff2414b33170
+size 10655
