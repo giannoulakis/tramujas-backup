595d0c4b542b182d3ffed51f12d2c49072df1cf0
Patrick Scheibel <patrick@ideatos.com.br>
2021-11-18 18:32:57 -0300

    Ajuste na migracao de contrato

--- resources/views/contratos.php
+++ resources/views/contratos.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c33bbb672bd1006f9f4120012715da4fdc11961411e0dfe9beaabea26bd5d1e8
-size 43576
+oid sha256:f6397a46ffa3b121e5e6cf67ad7d0d77f5f803034bedb12c991a8f2e5862b11d
+size 43527
