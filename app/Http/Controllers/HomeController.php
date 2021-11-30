cf8df193b4b1b3d372670941d02c06f99cdfd63d
Patrick Scheibel <patrick@ideatos.com.br>
2021-11-30 18:07:50 -0300

    Migracao do controller de home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd7d28ce49fe1ac7a03f4995552ecb071c6c53f2d22c262642f82e8a9b3b9732
-size 41391
+oid sha256:caec429086783fdf9288ca04ea1e44e85c28a3935bc74e81356eee8184edb060
+size 41839
