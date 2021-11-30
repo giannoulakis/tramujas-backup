07710734dacbe396b6df6fedf1174d8441ad33cb
Patrick Scheibel <patrick@ideatos.com.br>
2021-11-30 18:14:43 -0300

    Migracao do controller de procuracao

--- app/Http/Controllers/ProcuracaoController.php
+++ app/Http/Controllers/ProcuracaoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e336b60e662881b5c57e146fe4404b68de8a07e7b963aefc04e8081a292fdafa
-size 6136
+oid sha256:cd07479db12f176828cf8ddc7333d52815f8aabdd4a57e86f2b8cd5ebd1e61f8
+size 6342
