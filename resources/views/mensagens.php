169d0efc047a8764dd84616aba94b0690e77a23a
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-06 18:13:00 -0300

    Ajuste na view de mensagem e processo

--- resources/views/mensagens.php
+++ resources/views/mensagens.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03bb00029a8186ae3b444efdee1714fc070f345470323057b2a9e983e621d7b7
-size 7799
+oid sha256:fb44bdc938a6e41006fbc765f47c9b74786c46425f8c23b13da84e809f9b0359
+size 7852
