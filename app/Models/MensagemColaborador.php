72d44b595f21b7fbebf0b76b28b3ce991db61233
ideatos-patrick <patrick@ideatos.com.br>
2021-12-08 12:13:27 -0300

    Ajuste no model de mensagemColaborador

--- app/Models/MensagemColaborador.php
+++ app/Models/MensagemColaborador.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17e47c2a69f09d02d5b8b2a88ce46fda70364ff4ccab22f5f29a5ae268692e2e
-size 392
+oid sha256:9476da2708764baf3bbfa404b0dc77a47b2cf489b5d4fa97f51488e9cb49e2d0
+size 461
