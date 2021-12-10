cedc5cc9a3eacdec4102d111eed7827239d47d64
ideatos-patrick <patrick@ideatos.com.br>
2021-12-10 12:12:09 -0300

    Migracao da view de home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11b64b127f37cffa0994922118d7fb4734e597dfb4709cc053f6619e5ca546fa
+size 2611
--- resources/views/home.php
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:19da5a0514ead3f781631cde385550f2cd718044f218ecfdd003a19bb4ee1608
-size 2596
