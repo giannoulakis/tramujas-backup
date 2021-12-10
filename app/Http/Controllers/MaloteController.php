ae8080539274dd7e58f6cd9c82d0929a2169cbe3
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-10 17:01:16 -0300

    Ajuste no controller de malote

--- app/Http/Controllers/MaloteController.php
+++ app/Http/Controllers/MaloteController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b629a0c3b3f415e44c8b38cbf0a36b80ac2db803a963c6c75311e378875a7206
-size 22719
+oid sha256:c8f9686f67035d13cfd734460a274be8c4c58d0e41b7efa30b1d809a588d3919
+size 22725
