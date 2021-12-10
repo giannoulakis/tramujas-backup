2f3a0c1eeda2c5ae453873365dbee15576ea03ec
ideatos-patrick <patrick@ideatos.com.br>
2021-12-10 12:12:58 -0300

    Migracao da view de revisao

--- resources/views/revisao.php
+++ resources/views/revisao.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8d4ed5e8be95e6c81d462ef5dda26ef4cd89fb559d6e218ebafc4d4c80713f2
-size 5001
+oid sha256:5485ea30192b62e7047eadb83fa3b5f17311c6d3b33e49c4f92a3f321a13f507
+size 5072
