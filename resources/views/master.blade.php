20a46b8c193dabface0814786ffa8da4ced7176c
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-13 17:40:35 -0300

    Migracao da view de master

---
+++ resources/views/master.blade.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db257aeb80e0b146d7a738b7391775144ae3a17144e3363d373f666a26751a8e
+size 11915
--- resources/views/master.php
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aca973ac5debae56cde67b3df8bf16559773dee0d66e46ce4dce0c5cecc5c809
-size 9364
