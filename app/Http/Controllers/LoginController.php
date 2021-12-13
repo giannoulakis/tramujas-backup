9f7f9981f7cfa2b3fab0878ca0b217fd2d2dbbe0
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-13 17:45:23 -0300

    Ajuste no controller de login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e59357c51ac7cfc371542d0ae4de416a7fcc0fe4373dabba0261cf4d90c5386d
-size 1214
+oid sha256:445cf00caf920e61ce95f28e367469c3e6fa56d97349d43fe14c71bec071db0d
+size 1217
