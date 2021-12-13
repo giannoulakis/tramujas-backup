f2fac51828489d0e330875074d6f4bc9d35f1a20
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-13 18:13:14 -0300

    Ajuste no controller de boleto

--- app/Http/Controllers/BoletoController.php
+++ app/Http/Controllers/BoletoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6796af8a59e0bb68b8c2ecd0c55bd4239b1fae85bf18a1750a27100912a541f1
-size 75916
+oid sha256:3683b94d159c86a78f55d48915cb7c7646f6eb242e9d9c61e55c2e369278f0bf
+size 75910
