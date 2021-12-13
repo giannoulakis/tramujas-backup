85b0eb9622361be6e258beda701d90c6d566658b
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-13 18:18:08 -0300

    Ajuste no controller de cadastro

--- app/Http/Controllers/CadastrosController.php
+++ app/Http/Controllers/CadastrosController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0f4dfa423587d55ea42e6fef9052d3a37c610c14da9f86c959dbb461fd6f5ae
-size 9933
+oid sha256:01069c78048cab4dd865cc49154eafc78f53f7c5f2ec020a0ead5b6e761a172e
+size 9906
