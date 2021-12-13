e32343010d6faad784dd8df37efd8a41742dfd7f
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-13 18:27:18 -0300

    Ajuste no controller de financeiro

--- app/Http/Controllers/FinanceiroController.php
+++ app/Http/Controllers/FinanceiroController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b247f8ea5ecb90f1657b8e5dc8591ee2fe778e07005d3820ae6fa7e1c96bbac
-size 20039
+oid sha256:7f36dd240c14bf0cc63a971af1ce7308978d2f5eddd12f9349fc0cddc4210907
+size 20057
