a6bed07b73b76cc172d2f76cd70a8c87a7ed3627
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-13 18:37:02 -0300

    Ajuste no controller de pesquisa

--- app/Http/Controllers/PesquisaController.php
+++ app/Http/Controllers/PesquisaController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5db75ac2047f009014570b00140f57e9b632cc1adfee45e4521bbcd605d33715
-size 12516
+oid sha256:6d514adc6b9729b8e619b74fdaa6e7d740016553e5b5939aa115c903edec298c
+size 12547
