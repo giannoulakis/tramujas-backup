cb6fdf6516c7d8c5fb4ec59c3df5bbd323ba5ff4
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-13 18:39:51 -0300

    Ajuste na model de usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8085fa9ec83f7fc084364d0606e7b38eb840facaed5e80cd5b6b21a660412a86
-size 2530
+oid sha256:41f3ebd5c24dc91e94112e5c13efa9f37cecbb615c83ba0ed01dfd72da89dd71
+size 2799
