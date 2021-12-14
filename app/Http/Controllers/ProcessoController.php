7643036fdeb1bf0321beb0d1c125f29a9566ed4a
ideatos-patrick <patrick@ideatos.com.br>
2021-12-14 11:34:23 -0300

    Ajuste no controller de processo

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9268999c85247f4706e409f5f1ead5f9745c4ea3b64fea9ce82a271a93745250
-size 74526
+oid sha256:be9befebe789db21f3ecf2ac0488e7bbb37ea041984e579a225fb09c62a08208
+size 74524
