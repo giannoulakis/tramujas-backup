6bdcd15f896b952d7c4363f2017bdbcb43c9bc18
ideatos-patrick <patrick@ideatos.com.br>
2021-12-14 11:31:01 -0300

    Ajuste no controller de usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d80f78aaf983dc625925475e92e9db6d256dc0b849aef381b33cac44733c0ec
-size 5247
+oid sha256:a16cbc6e053b9c35d9cc2707d1cb04eb2888915d96cb364341a925e8e44401e9
+size 5235
