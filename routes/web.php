035de1729af825ef2faf7001a656a98902e8237e
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-15 18:34:00 -0300

    Adicionada nova rota de entidade

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70f3554800737cd6c3f6b3ce871f9dcf260ba10868284c9ccdbe65079c3ea03e
-size 13919
+oid sha256:7012c1cae89b3d13060722a77f784de4bf23bba94136be9bb2f6258f1702b081
+size 14121
