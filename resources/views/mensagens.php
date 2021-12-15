b220f50e7047eb7414f536eafe783e2113fa052b
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-15 18:31:16 -0300

    Ajuste na utilizacao do auth nas views

--- resources/views/mensagens.php
+++ resources/views/mensagens.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb44bdc938a6e41006fbc765f47c9b74786c46425f8c23b13da84e809f9b0359
-size 7852
+oid sha256:db053db56461cd2095b7503a001a66a072444f304e82a12f20e71c423d34fb55
+size 7896
--- resources/views/pesquisas.php
+++ resources/views/pesquisas.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5ff9afcf7367f4f45314f9ce3a7a51404236a9cd0a517ad7dbacf83b938dcd1
-size 23476
+oid sha256:623dc6344dee0b497e0d4f1e918f43e575d027fc5b474b6200f7558064d72a3b
+size 23479
