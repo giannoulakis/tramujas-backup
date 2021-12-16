03f28152d4ce0d18f4ae748ae949f8f3b168f955
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-16 19:14:26 -0300

    Ajuste no controller de entidade

--- app/Http/Controllers/EntidadeController.php
+++ app/Http/Controllers/EntidadeController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af8e968e6068f69e27023b50b1469d136f2df1f2dedd59506d89caf3cace7e1c
-size 10991
+oid sha256:14efa3da6a6699c82f2c525eb4a441595a0d6c88221cc34f18887e7f88013444
+size 11010
