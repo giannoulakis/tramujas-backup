e5a36fb85a64efc28f6881c6fe37df9a8ab96de1
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-16 19:16:17 -0300

    Ajuste no controller de relatorio

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:616cecae32f6e872d498b97cb0012f4f375e79b6206d86c486a9e620e03acc30
-size 33530
+oid sha256:0aedb3ced31f55575e9d2cc0b19cad9b09b76a2e3c4686d1925a347fc1aaf1f7
+size 33419
