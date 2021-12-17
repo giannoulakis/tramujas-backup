43c46309ed2915b087736644ac3a593fdead3496
Patrick Scheibel <patrick@ideatos.com.br>
2021-12-17 20:21:34 -0300

    Adicionado required no campo de data de vencimento

--- resources/views/boletos.php
+++ resources/views/boletos.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aeb9ebd972a1019cca38f5552b719e34331f5b89e4e19da710c23eb53f076c00
-size 18305
+oid sha256:32fb9790b4f8548b5897d2ae129acf3514d1ca70ae679bf59d4f5729f5e1441f
+size 18314
