360ac83d07a0d45cc57af88437ef0e4370d0a780
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-02-02 15:24:14 -0300

    Limite de registros na listagem de entidades

--- app/Http/Controllers/EntidadeController.php
+++ app/Http/Controllers/EntidadeController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c52e2c0078a3b2372c7a3c7fb176857634117597a8d0b9427b66f407f6991f7
-size 10860
+oid sha256:0c066a5c0700c5f838fbbe6dfd77d7d248ce746ceca8b05fbb33ece29ef423bc
+size 10978
