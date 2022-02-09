5506a8efe39c264f967a09a515843cf4748dc907
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-02-09 10:23:46 -0300

    Ordenação das pesquisas pelo id por padrão

--- app/Http/Controllers/PesquisaController.php
+++ app/Http/Controllers/PesquisaController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d514adc6b9729b8e619b74fdaa6e7d740016553e5b5939aa115c903edec298c
-size 12547
+oid sha256:246f4c7d2da5a8b5827c079121ed25adff795467da345504ada9211de16d0416
+size 12545
--- public/js/app.pesquisas.js
+++ public/js/app.pesquisas.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33930755045c797fbfcb967c7a24b735877f260c341cacef329d1e070e8f195e
-size 13113
+oid sha256:99e4f22383ea26e822af5f5d72f5ba0bef8055d3bb6a86682503d3a1055ad35b
+size 13112
