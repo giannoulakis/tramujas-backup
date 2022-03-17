b9507d91df667d622f134444249fbb9a2c9969e8
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-03-17 16:21:18 -0300

    Valor null default para a proposta

--- public/js/app.contratos.js
+++ public/js/app.contratos.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05f8977953a0c9953a659df01926890004f02e26a686363d8c94f3b3f77a3af0
-size 27852
+oid sha256:aa75fbc7eb88bf135c21bff99b487a4994e6eeef1cddffab102ebe3b6929d4df
+size 27975
