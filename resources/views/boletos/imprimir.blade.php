1a40120ae7f7de638db55e6e275a5fe80addae6c
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-03-31 14:43:49 -0300

    Ajuste no layout do boleto

--- resources/views/boletos/imprimir.blade.php
+++ resources/views/boletos/imprimir.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41626a20c188af6278295dc5bd47a8b38a5f86bf08afa0681633f49c838ab714
-size 19912
+oid sha256:339ae7900128b2464ba06fad313bb8586da456a720b3720bd4b1bed24ad7cc80
+size 19639
