e5c9b1ed146e32086185284909fdfa72bdf74347
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-04-01 16:44:49 -0300

    alexandre.ideatos

--- resources/views/boletos/imprimir.blade.php
+++ resources/views/boletos/imprimir.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b426124953271d985f5aef389e3a91d1bde43211e15a4464bf5162fb92a2a789
+oid sha256:4a614c85962676b48d7926f92fcc67d6f5bb936a1d766e54dc90d4a3fc51f8a0
 size 19639
