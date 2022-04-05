22ff100cd4d5864b3c31919c3bece48145f71faf
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-04-05 11:29:17 -0300

    Mensagens não estavam marcando como lida/não lida

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cfc9a66badcb126e5a2e6df1eec1791535f94ee38353b77c470fa2f508cce86
-size 18115
+oid sha256:ad24b74041911ca97033faea79627e227e59b3b6fcbce1f20488d1d780da293e
+size 18380
