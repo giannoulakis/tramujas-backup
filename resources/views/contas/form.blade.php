5b558595de8826b93b48bced4b704c4e0d2799f8
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-04-06 10:41:26 -0300

    Ajuste no valor pago das parcelas

--- resources/views/contas/form.blade.php
+++ resources/views/contas/form.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f90e1924e84a4d7a556bc77e298e733ab59acc4cea95769bd1c43034c9b9788a
-size 36830
+oid sha256:77bb22e594e9351a7b9e9fb6188e392471b9e2d195f060b640ca621d3a5ebec0
+size 36843
