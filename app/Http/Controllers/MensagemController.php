cb0094e6e498b8fa3b7650fe8170b4ad4b1827bf
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-04-07 13:58:40 -0300

    Versão do PHP causou erro na consulta da mensagens

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad24b74041911ca97033faea79627e227e59b3b6fcbce1f20488d1d780da293e
-size 18380
+oid sha256:1d8b8e79e7ea581506a4d9243a6732a7c4009819d27e361959ad1958088be4d7
+size 18414
