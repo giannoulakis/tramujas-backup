85c96fe2988e6d10be07a7ea30dfe5155dde2f68
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-04-11 11:43:36 -0300

    Ajuste no layout do PDF de contrato

--- app/Http/Controllers/ContratoController.php
+++ app/Http/Controllers/ContratoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ac15dacfa3755a1ceb4ff735f44b66f735b5a5dd382ed9e4f2408559115c4e4
-size 47271
+oid sha256:e4cbb71e1500b0942654fd717f6bafbeeb9cdfe99497e59666aaab4610ec82c4
+size 47223
