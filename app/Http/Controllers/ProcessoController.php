2828f8157d6a385b7853929f2de6ad7947f43dca
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-04-25 15:54:39 -0300

    Remoção dos usos do DATE_ATOM

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9a56daf87cfd41ecaf4437be64e1472ea1bb75df04e70f7567d715e734d4661
-size 74372
+oid sha256:e264ee031a403929ef7708c8841f22460240dc591cd08ec8d54a7f4fb5784e62
+size 74385
