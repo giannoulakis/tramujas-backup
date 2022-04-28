e1cbfdf3b33393e204181f017ae849df9a66dfd3
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-04-28 18:00:32 -0300

    Remoção da busca automática na tela de processos

--- public/js/app.processos.js
+++ public/js/app.processos.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5bc6417121da42badfd8146103a594c41f57461596ac999ba460e356c95b240
-size 71019
+oid sha256:b79cec125f899fdf4b416a2b481e2015843f80069cfb8dc980ff19dc6480ed71
+size 71021
