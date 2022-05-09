ab608de359958d239a2b32e54724b6f01d036aa1
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-09 12:06:14 -0300

    Ajuste nos filtros dos processos

--- resources/views/processos.php
+++ resources/views/processos.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:896f82052968d09c8ffa0cf9b1f9c4547b4eef29b491a333c1a76eafc19cf0e0
-size 111504
+oid sha256:6a9fab05f8ed46600c0d0a15a2ac2fe4770659e838cd93214e97d7b42dd1aa79
+size 112699
