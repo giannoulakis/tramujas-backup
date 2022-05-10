e06970e261640e25257ee67b944c7fa392b78331
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-10 11:04:07 -0300

    Assinatura dos contratantes estavam sobrepostos

--- app/Http/Controllers/ContratoController.php
+++ app/Http/Controllers/ContratoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cba9476337d15d89725a96029bc88db5d483c0d3e9a8ef9369533b154d5f1482
-size 47251
+oid sha256:8f857d22b82406ea373066fa4f0339691b6cecad1c491d7c2c6f513b5c1cfe8a
+size 47304
