aab36dd3621ca42b90d13a9e79d89ea5a8c6283e
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-10 16:07:07 -0300

    Ajuste nas assinaturas dos contratos

--- app/Http/Controllers/ContratoController.php
+++ app/Http/Controllers/ContratoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f857d22b82406ea373066fa4f0339691b6cecad1c491d7c2c6f513b5c1cfe8a
-size 47304
+oid sha256:85b627911c2d9bfd2369bc0d7addd8b6660a2cc6ac0a8bf29402624a9d8b1057
+size 47540
