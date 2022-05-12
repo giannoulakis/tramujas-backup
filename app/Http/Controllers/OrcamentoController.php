6e27f7c9bfead6e0a58dc75d44ad85910673c9e2
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-12 15:14:16 -0300

    Ajuste no envio de orcamento por email

--- app/Http/Controllers/OrcamentoController.php
+++ app/Http/Controllers/OrcamentoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16df18f547c6c06a5d2f98135ccff615dc34ba809c9a6ee97ef327c6fd6fcca7
-size 29441
+oid sha256:dd0308324247e7ccb3f422b623d56f4e884ee90bd5e71f196a65b7684cf0b289
+size 29439
