95df6a2e93db8c7ceada330c7f564f85afa6b0a3
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-13 10:24:33 -0300

    Botão do whatsapp deve aparecer sempre

--- resources/views/orcamentos.php
+++ resources/views/orcamentos.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f86a1483b978ff5e87e9412b6e4e813a72d45730dc9b4918adeb1e1ba235b16
-size 38313
+oid sha256:84a4d36296387e2fd9119664a208118aa0da1b5ebb712ea7d193236f7b730a4f
+size 38147
