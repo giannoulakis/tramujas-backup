a3047bdd7ef4a21dd8d94ad2d5fb583957a0faea
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-16 08:39:14 -0300

    Ajuste nos comandos para a versão 8 do laravel

--- app/Console/Commands/migrateS3.php
+++ app/Console/Commands/migrateS3.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49bf9d11a26dc45572b6ab3933e9819fa2c84ad984fdefc4cf2ec0889f448fc7
-size 3808
+oid sha256:01bcda0f96b5952bcebf752e3e8be1567f8355efd0e55338dbe76a5fa3fe5bae
+size 3796
