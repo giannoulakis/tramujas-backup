fdf331334d6eee8c571cddeb6cfe41317d68b0d0
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-19 17:43:52 -0300

    Ajuste na data dos malotes

--- app/Http/Controllers/MaloteController.php
+++ app/Http/Controllers/MaloteController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:825e1152060ec01645fa6e022e5986ae18cf0b33843f8bb976e1b25a35e2a952
-size 21090
+oid sha256:4cabbca4941ecc8a2fe861b8759763380bf9ef84fd56e80850c480330dfa6089
+size 21135
