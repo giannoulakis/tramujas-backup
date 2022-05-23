a4d6d75ef1b14ea34aa6343c73062138974bdb0b
Alexandre Klabunde <alexandre@ideatos.com.br>
2022-05-23 16:46:50 -0300

    Ajuste na validação de documentos pendentes

--- public/js/app.processos.js
+++ public/js/app.processos.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9dd3717eeaf379c23661be8ca16f39f7b8af9e25530cee45ca64bec9254dfb9
-size 71701
+oid sha256:4d400a72b1886a6df1e96c018d92a38db7dea9b9900f3b0207a8782eaf9df7c5
+size 71778
