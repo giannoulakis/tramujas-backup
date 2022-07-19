7b23c5cb99a1d0db79f59e785b5b35dc6daee80b
gabriel.lutz <gabriel.lutz@retta.com.br>
2022-07-19 15:54:24 -0300

    ajuste: alterando lista de e-mails de copia oculta para copia normal

--- app/Http/Controllers/ContratoController.php
+++ app/Http/Controllers/ContratoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1689f2f8d8b87b75f57a48db5f42c79b2991dc6306af483d41ad5e9e28b5d134
-size 48023
+oid sha256:110fb022d815a891990a2a0a77f59207b96fb488357885a6af35ac3f84fe4c91
+size 48028
