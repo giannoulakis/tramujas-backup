ff4e0aaa72a308eb0a27c9604c77568fb702d895
Samuel <samuel@retta.com.br>
2022-09-14 11:54:19 -0300

    Ajuste na mensagem https://redmine.retta.com.br/issues/100550

--- app/Http/Controllers/OrcamentoController.php
+++ app/Http/Controllers/OrcamentoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b03bf7dec541f529523be191fe031e72d42b4ea8e931608847a743374068d1cd
-size 30437
+oid sha256:172f84cc45edbc355e6ff7617a486c4a451d8257da61efe6c4a175b36797b891
+size 30425
