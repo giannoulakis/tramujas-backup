0f4cdd06a5b15fd5e5eba275c3f34b4ad1dd7f86
Samuel <samuel@retta.com.br>
2022-10-27 15:01:36 -0300

    Ajuste create fase REF https://redmine.retta.com.br/issues/103562

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd992fc4b1e2c84a1e466e5d0361418c35c54e3929b1767cfd5adc6f100ea5f6
-size 111300
+oid sha256:0c6bf3bf06b6dbf89235dbe3182467b0e811b851085d0804a944c6bbc33b0336
+size 111334
